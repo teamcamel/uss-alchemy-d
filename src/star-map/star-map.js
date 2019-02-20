0490d999e8579ec863fc0ebe7dd862b33b41ffa1
Cheri Portman <cheriportman@hotmail.com>
2019-02-19 16:34:44 -0800

    started making the planets clickable sequentialy

--- src/star-map/star-map.js
+++ src/star-map/star-map.js
@@ -1,3 +1,27 @@
 import statusBar from '../functions/status-bar.js';
+import { getUserJson, setUserJson } from '../../../Utilities/jsonFunction.js';
+
+const imageMap = document.getElementById('image-map');
+const user = getUserJson();
+console.log('userJson', getUserJson());
+
+const trueFalse = true;
+
+if(trueFalse === true) {
+    const planetTwo = document.createElement('area');
+    planetTwo.id = 'tribbles';
+    planetTwo.target = '_self';
+    planetTwo.alt = 'game two: tribbles';
+    planetTwo.title = 'Tribbles game';
+    planetTwo.href = 'tribbles.html';
+    planetTwo.coords = '359,420,93';
+    planetTwo.shape = 'circle';
+
+    console.log('image map', imageMap);
+    console.log('planet Two', planetTwo);
+    imageMap.appendChild(planetTwo);
+
+
+}
 
 statusBar();
