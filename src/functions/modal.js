a44a72487fbf560bf01527b43b46a10380ee8da1
Cheri Portman <cheriportman@hotmail.com>
2019-02-20 20:23:24 -0800

    working modal. moving to a module.

---
+++ src/functions/modal.js
@@ -0,0 +1,22 @@
+function makeModal() {
+    const modal = document.querySelector('.modal');
+    const trigger = document.querySelector('.trigger');
+    const closeButton = document.querySelector('.close-button');
+    
+    function toggleModal() {
+        modal.classList.toggle('show-modal');
+    }
+    
+    function windowOnClick(event) {
+        if(event.target === modal) {
+            toggleModal();
+        }
+    }
+    
+    trigger.addEventListener('click', toggleModal);
+    closeButton.addEventListener('click', toggleModal);
+    window.addEventListener('click', windowOnClick);
+
+}
+
+export default makeModal;
